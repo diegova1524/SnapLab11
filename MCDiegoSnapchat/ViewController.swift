ff3819845c188e8ed2a5ff24d490ef7ff5ab92b8
Diego Valerio <diego.mamani.co@tecsup.edu.pe>
2024-05-29 09:20:30 -0500

    Initial Commit

---
+++ MCDiegoSnapchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MCDiegoSnapchat
+//
+//  Created by Diego Valerio Mamani Coila  on 29/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
